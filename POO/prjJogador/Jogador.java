d3688a807d205f1a574565c7b7b3b92a75685341
juliana15paak <112185575+juliana15paak@users.noreply.github.com>
2023-03-22 20:39:13 -0300

    Add files via upload
    Programação Orientada à Objetos

---
+++ POO/prjJogador/Jogador.java
@@ -0,0 +1,17 @@
+package prjJogador;
+
+public class Jogador {
+	String nome;
+	String posicão;
+	String time;
+	String idade;
+	
+		public void chutar() {
+			System.out.println("CHUTEEEEEEEIII!!");
+		
+		}
+		public void gol() {
+			System.out.println("GOOOOOOLLLL!!");
+		}
+	
+}
